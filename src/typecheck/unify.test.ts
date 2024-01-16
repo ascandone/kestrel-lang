fb0eeb688ddedc84014403e834fb8fe7e63b40d2
Alessandro Scandone <alescandone@gmail.com>
2024-01-16 20:13:28 +0100

    partially implemented unify

---
+++ src/typecheck/unify.test.ts
@@ -0,0 +1,476 @@
+import { test, expect, beforeEach, describe } from "vitest";
+import {
+  ConcreteType,
+  TVar,
+  TVarResolution,
+  Type,
+  UnifyError,
+  generalize,
+  instantiate,
+  unify,
+} from "./unify";
+
+beforeEach(() => {
+  TVar.resetId();
+});
+
+test("unifing two concrete vars when they match", () => {
+  expect(unify(Int, Int)).toBeUndefined();
+  expect(unify(List(Int), List(Int))).toBeUndefined();
+});
+
+test("unify two concrete vars that do not match", () => {
+  expect(unify(Int, Bool)).toEqual<UnifyError>({
+    type: "type-mismatch",
+    left: Int,
+    right: Bool,
+  });
+  expect(unify(Tuple(Int, Int), Tuple())).not.toBeUndefined();
+  expect(unify(List(Int), List(Bool))).not.toBeUndefined();
+});
+
+test("TypeVar is unbound initially", () => {
+  const $a = TVar.fresh();
+
+  expect($a.resolve()).toEqual<TVarResolution>({
+    type: "unbound",
+    id: 0,
+  });
+});
+
+test("unify a concrete type and a var", () => {
+  const $a = TVar.fresh();
+  unify($a.asType(), Int);
+
+  expect($a.resolve()).toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+});
+
+test("unify a var and a concrete type", () => {
+  const $a = TVar.fresh();
+  unify(Int, $a.asType());
+
+  expect($a.resolve()).toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+});
+
+test("unify to another TVar", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  unify($a.asType(), $b.asType());
+  expect($a.resolve()).toEqual<TVarResolution>($b.resolve());
+});
+
+test("unify to another bound TVar should fail", () => {
+  const $a = TVar.fresh();
+  unify($a.asType(), Int);
+
+  const $b = TVar.fresh();
+  unify($b.asType(), Bool);
+
+  const $c = TVar.fresh();
+  unify($b.asType(), $c.asType());
+
+  expect(unify($a.asType(), $c.asType())).toEqual<UnifyError>({
+    type: "type-mismatch",
+    left: Bool,
+    right: Int,
+  });
+});
+
+test("unify 3 lvars", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  const $c = TVar.fresh();
+
+  unify($a.asType(), $c.asType());
+  unify($c.asType(), $b.asType());
+  unify($a.asType(), Int);
+
+  expect($a.resolve()).toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+  expect($b.resolve()).toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+  expect($c.resolve()).toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+});
+
+test("TVars should be reactive (left)", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+
+  unify($a.asType(), $b.asType());
+  unify($b.asType(), Int);
+
+  expect($a.resolve(), "a").toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+  expect($b.resolve(), "b").toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+});
+
+test("TVars should be reactive (right)", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+
+  unify($b.asType(), $a.asType());
+  unify($b.asType(), Int);
+
+  expect($a.resolve(), "a").toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+  expect($b.resolve(), "b").toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+});
+
+test("trying to link a linked var (1)", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  const $c = TVar.fresh();
+
+  unify($c.asType(), $a.asType()); // a~>c
+  unify($b.asType(), $a.asType()); // a~>b
+  unify($b.asType(), Int); // b=Int
+
+  expect($a.resolve(), "a").toEqual({ type: "bound", value: Int });
+  expect($b.resolve(), "b").toEqual({ type: "bound", value: Int });
+  expect($c.resolve(), "c").toEqual({ type: "bound", value: Int });
+});
+
+test("trying to link a linked var (2)", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  const $c = TVar.fresh();
+
+  unify($a.asType(), $c.asType());
+  unify($a.asType(), $b.asType());
+  unify($b.asType(), Int);
+
+  expect($a.resolve(), "a").toEqual({ type: "bound", value: Int });
+  expect($b.resolve(), "b").toEqual({ type: "bound", value: Int });
+  expect($c.resolve(), "c").toEqual({ type: "bound", value: Int });
+});
+
+test("trying to unify two linked vars", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  const $c = TVar.fresh();
+  const $d = TVar.fresh();
+
+  unify($b.asType(), $a.asType()); // a~>b
+  unify($d.asType(), $c.asType()); // c~>d
+  unify($a.asType(), $c.asType()); // c~>a
+
+  unify($b.asType(), Int);
+
+  expect($a.resolve(), "a").toEqual({ type: "bound", value: Int });
+  expect($b.resolve(), "b").toEqual({ type: "bound", value: Int });
+  expect($c.resolve(), "c").toEqual({ type: "bound", value: Int });
+  expect($d.resolve(), "d").toEqual({ type: "bound", value: Int });
+});
+
+test("unify to another TVar", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  unify($a.asType(), $b.asType());
+  expect($a.resolve()).toEqual<TVarResolution>({ type: "unbound", id: 0 });
+  expect($a.resolve()).toEqual($b.resolve());
+});
+
+test("unify nested TVar", () => {
+  const $a = TVar.fresh();
+  unify(List($a.asType()), List(Bool));
+  expect($a.resolve()).toEqual<TVarResolution>({
+    type: "bound",
+    value: Bool,
+  });
+});
+
+test("unify twice to a const", () => {
+  const $a = TVar.fresh();
+
+  unify($a.asType(), Int);
+  unify($a.asType(), Int);
+
+  expect($a.resolve()).toEqual<TVarResolution>({
+    type: "bound",
+    value: Int,
+  });
+});
+
+test("unify to a const, then to a different one should fail", () => {
+  const $a = TVar.fresh();
+
+  unify($a.asType(), Int);
+  expect(unify($a.asType(), Bool)).toEqual<UnifyError>(
+    expect.objectContaining({ type: "type-mismatch" }),
+  );
+});
+
+test("transitive unifications", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  unify($a.asType(), $b.asType());
+  unify($a.asType(), Int);
+
+  expect($a.resolve(), "a == Int").toEqual({
+    type: "bound",
+    value: Int,
+  });
+  expect($b.resolve(), "b == Int").toEqual({
+    type: "bound",
+    value: Int,
+  });
+});
+
+test("transitive unifications (3)", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  const $c = TVar.fresh();
+
+  unify($a.asType(), $b.asType());
+  unify($b.asType(), $c.asType());
+
+  expect($a.resolve(), "a").toEqual<TVarResolution>({ type: "unbound", id: 0 });
+  expect($b.resolve(), "b").toEqual<TVarResolution>({ type: "unbound", id: 0 });
+  expect($b.resolve(), "c").toEqual<TVarResolution>({ type: "unbound", id: 0 });
+
+  unify($a.asType(), Bool);
+
+  expect($a.resolve(), "a == true").toEqual<TVarResolution>({
+    type: "bound",
+    value: Bool,
+  });
+  expect($b.resolve(), "b == true").toEqual<TVarResolution>({
+    type: "bound",
+    value: Bool,
+  });
+  expect($c.resolve(), "b == true").toEqual<TVarResolution>({
+    type: "bound",
+    value: Bool,
+  });
+});
+
+test("recursively linked TVars", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+
+  unify($a.asType(), $b.asType());
+  unify($b.asType(), $a.asType());
+
+  expect($a.resolve()).toEqual($b.resolve());
+});
+
+test("recursively linked TVars (3 steps)", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+  const $c = TVar.fresh();
+
+  unify($a.asType(), $b.asType());
+  unify($b.asType(), $c.asType());
+  unify($c.asType(), $a.asType());
+
+  expect($a.resolve()).toEqual($b.resolve());
+  expect($a.resolve()).toEqual($c.resolve());
+});
+
+test("occurs check", () => {
+  const $a = TVar.fresh();
+  expect(unify($a.asType(), List($a.asType()))).toEqual<UnifyError>({
+    type: "occurs-check",
+    left: $a.asType(),
+    right: List($a.asType()),
+  });
+});
+
+test("occurs check of unified values", () => {
+  const $a = TVar.fresh();
+  const $b = TVar.fresh();
+
+  unify($a.asType(), $b.asType());
+
+  expect(unify($a.asType(), List($b.asType()))).toEqual<UnifyError>({
+    type: "occurs-check",
+    left: $a.asType(),
+    right: List($b.asType()),
+  });
+});
+
+describe("generalization", () => {
+  test("generalize primitive value", () => {
+    const poly = generalize(Int);
+    expect(poly).toEqual(Int);
+  });
+
+  test("generalize var bound to primitive", () => {
+    const $a = TVar.fresh();
+    unify($a.asType(), Int);
+    const poly = generalize($a.asType());
+    expect(poly).toEqual(Int);
+  });
+
+  test("generalize single unbound var", () => {
+    const $a = TVar.fresh();
+    const poly = generalize($a.asType());
+    expect((poly as any).var.resolve().type).toEqual("quantified");
+  });
+
+  test("generalize many vars", () => {
+    const $a = TVar.fresh();
+    const $b = TVar.fresh();
+    const poly = generalize(Tuple($a.asType(), $b.asType()));
+
+    if (poly.type !== "named") {
+      throw new Error("fail");
+    }
+    expect(poly.args.length).toEqual(2);
+    expect(poly.name).toBe("Tuple");
+
+    const [$g1, $g2] = poly.args;
+
+    expect(($g1 as any).var.resolve().type).toEqual("quantified");
+    expect(($g1 as any).var.resolve().id).toEqual(0);
+
+    expect(($g2 as any).var.resolve().type).toEqual("quantified");
+    expect(($g2 as any).var.resolve().id).toEqual(1);
+  });
+
+  test("generalize many vars when linked", () => {
+    const $a = TVar.fresh();
+    const poly = generalize(Tuple($a.asType(), $a.asType()));
+
+    if (poly.type !== "named") {
+      throw new Error("fail");
+    }
+    expect(poly.args.length).toEqual(2);
+    expect(poly.name).toBe("Tuple");
+    const [$g1, $g2] = poly.args;
+
+    expect(($g1 as any).var.resolve().type).toEqual("quantified");
+    expect(($g1 as any).var.resolve().id).toEqual(0);
+
+    expect(($g2 as any).var.resolve().type).toEqual("quantified");
+    expect(($g2 as any).var.resolve().id).toEqual(0);
+  });
+
+  test("generalize var bound to a nested type to generalize ", () => {
+    const $a = TVar.fresh();
+    const $b = TVar.fresh();
+    unify($a.asType(), Tuple($b.asType(), $b.asType()));
+    const poly = generalize($a.asType());
+    if (poly.type !== "named") {
+      throw new Error("Expected a named type, got: " + poly.type);
+    }
+
+    expect(poly.args.length).toEqual(2);
+
+    const [$g1, $g2] = poly.args;
+
+    expect(($g1 as any).var.resolve()).toEqual({ type: "quantified", id: 0 });
+    expect(($g2 as any).var.resolve()).toEqual({ type: "quantified", id: 0 });
+  });
+
+  test("do not generalize vars that appear in the context", () => {
+    const $a = TVar.fresh();
+    const $b = TVar.fresh();
+    const poly = generalize(Tuple($a.asType(), $b.asType()), {
+      // Note $b is not free in context
+      x: List($b.asType()),
+    });
+
+    if (poly.type !== "named") {
+      throw new Error("Expected a named type, got: " + poly.type);
+    }
+
+    expect(poly.args.length).toEqual(2);
+    const [$ga, $gb] = poly.args;
+    expect(($ga as any).var.resolve()).toEqual({ type: "quantified", id: 0 });
+    expect($gb).toEqual($b.asType());
+  });
+
+  test("instantiate concrete type", () => {
+    const m = instantiate(Int);
+    expect(m).toEqual(Int);
+  });
+
+  test("instantiate single var", () => {
+    const $a = TVar.fresh();
+    const $g = generalize($a.asType());
+    const $m = instantiate($g);
+
+    expect(($m as any).var.resolve().type).toEqual("unbound");
+    expect((($m as any).var.resolve() as any).id).not.toEqual(
+      ($a.resolve() as any).id,
+    );
+  });
+
+  test("instantiate two different vars", () => {
+    const $a = TVar.fresh();
+    const $b = TVar.fresh();
+    const $g = generalize(Tuple($a.asType(), $b.asType()));
+
+    const m = instantiate($g);
+    if (m.type !== "named") {
+      throw new Error("fail");
+    }
+
+    const [$ai, $bi] = m.args;
+
+    expect(($ai as any).var.resolve().type).toEqual("unbound");
+    expect(($bi as any).var.resolve().type).toEqual("unbound");
+    expect(($ai as any).var.resolve().id).not.toEqual(
+      ($bi as any).var.resolve().id,
+    );
+  });
+
+  test("instantiate two same vars", () => {
+    const $a = TVar.fresh();
+    const $g = generalize(Tuple($a.asType(), $a.asType()));
+    const m = instantiate($g);
+    if (m.type !== "named") {
+      throw new Error("fail");
+    }
+
+    const [$ai, $bi] = m.args;
+
+    expect(($ai as any).var.resolve().type).toEqual("unbound");
+    expect(($bi as any).var.resolve().type).toEqual("unbound");
+    expect(($ai as any).var.resolve().id).toEqual(
+      ($bi as any).var.resolve().id,
+    );
+  });
+});
+
+function named(name: string, ...args: Type[]): ConcreteType {
+  return { type: "named", name, args };
+}
+
+function $(): Type {
+  return { type: "var", var: TVar.fresh() };
+}
+
+const Int = named("Int");
+const Bool = named("Bool");
+function List(p: Type) {
+  return named("List", p);
+}
+
+function Tuple(...ps: Type[]): ConcreteType {
+  return named("Tuple", ...ps);
+}
---
+++ src/typecheck/unify.ts
@@ -0,0 +1,283 @@
+export type ConcreteType =
+  | {
+      type: "fn";
+      args: Type[];
+      return: Type;
+    }
+  | {
+      type: "named";
+      name: string;
+      args: Type[];
+    };
+
+export type Type =
+  | ConcreteType
+  | {
+      type: "var";
+      var: TVar;
+    };
+
+export type TVarResolution =
+  | { type: "unbound"; id: number }
+  | { type: "bound"; value: ConcreteType }
+  | { type: "quantified"; id: number };
+
+export type UnifyError = {
+  type: UnifyErrorType;
+  left: Type;
+  right: Type;
+};
+
+export class TVar {
+  private constructor(
+    private value: TVarResolution | { type: "linked"; to: TVar },
+  ) {}
+
+  static fresh(): TVar {
+    return new TVar({ type: "unbound", id: TVar.unboundId++ });
+  }
+
+  static quantified(id: number): TVar {
+    return new TVar({ type: "quantified", id });
+  }
+
+  resolve(): TVarResolution {
+    if (this.value.type === "linked") {
+      return this.value.to.resolve();
+    }
+
+    return this.value;
+  }
+
+  private static unboundId = 0;
+  static resetId() {
+    TVar.unboundId = 0;
+  }
+
+  asType(): Type {
+    return { type: "var", var: this };
+  }
+
+  static unify(t1: Type, t2: Type): UnifyError | undefined {
+    // (Var, Var)
+    if (t1.type === "var" && t2.type === "var") {
+      return TVar.unifyVars(t1.var, t2.var);
+    }
+
+    // (Var, _)
+    if (t1.type === "var" && t2.type !== "var") {
+      const occurs = occursCheck(t1.var, t2);
+      if (occurs) {
+        return {
+          type: "occurs-check",
+          left: t1.var.asType(),
+          right: t2,
+        };
+      }
+
+      switch (t1.var.value.type) {
+        case "bound":
+          return TVar.unify(t1.var.value.value, t2);
+        case "unbound":
+          t1.var.value = { type: "bound", value: t2 };
+          return;
+        case "linked":
+          return TVar.unify(t1.var.value.to.asType(), t2);
+        default:
+          throw new Error("[unreachable]");
+      }
+    }
+
+    // (_, Var)
+    if (t2.type === "var") {
+      return TVar.unify(t2, t1);
+    }
+
+    // (Named, Named)
+    if (t1.type === "named" && t2.type === "named") {
+      if (t1.name !== t2.name || t1.args.length !== t2.args.length) {
+        return { type: "type-mismatch", left: t1, right: t2 };
+      }
+
+      for (let i = 0; i < t1.args.length; i++) {
+        const res = TVar.unify(t1.args[i]!, t2.args[i]!);
+        if (res !== undefined) {
+          return res;
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+  private static unifyVars($1: TVar, $2: TVar): UnifyError | undefined {
+    const r1 = $1.resolve();
+    if (r1.type === "bound") {
+      return TVar.unify($2.asType(), r1.value);
+    }
+
+    const r2 = $2.resolve();
+    if (r2.type === "bound") {
+      return TVar.unify($1.asType(), r2.value);
+    }
+
+    if ($2.value.type === "linked") {
+      return TVar.unify($2.value.to.asType(), $1.asType());
+    }
+
+    if (r1.type === "unbound" && r2.type === "unbound" && r1.id === r2.id) {
+      // TVars are already linked
+      return;
+    }
+
+    $2.value = { type: "linked", to: $1 };
+    return undefined;
+  }
+}
+
+export type UnifyErrorType = "type-mismatch" | "occurs-check";
+
+export const unify = TVar.unify;
+
+// Occurs check on monotypes
+function occursCheck(v: TVar, x: Type): boolean {
+  if (x.type === "named") {
+    return x.args.some((a) => occursCheck(v, a));
+  }
+
+  if (x.type === "fn") {
+    throw new Error("TODO fn occurs check");
+  }
+
+  const resolvedV = v.resolve();
+  if (resolvedV.type === "quantified") {
+    throw new Error("[unreachable] 0");
+  }
+
+  if (resolvedV.type === "bound") {
+    return false;
+  }
+
+  const resolvedX = x.var.resolve();
+  if (resolvedX.type === "quantified") {
+    throw new Error("[unreachable] 2");
+  }
+
+  if (resolvedX.type === "bound") {
+    return false;
+  }
+
+  if (resolvedV.id === resolvedX.id) {
+    return true;
+  }
+
+  return false;
+}
+export type Context = Record<string, Type>;
+
+function* getTypeFreeVars(t: Type): Generator<number> {
+  if (t.type === "var") {
+    const resolved = t.var.resolve();
+    if (resolved.type === "unbound") {
+      yield resolved.id;
+    }
+    return;
+  }
+
+  if (t.type === "named") {
+    for (const arg of t.args) {
+      yield* getTypeFreeVars(arg);
+    }
+    return;
+  }
+
+  if (t.type === "fn") {
+    throw new Error("TODO generalize fn");
+  }
+}
+
+/** Returns the set of ids of free vars in a context  */
+function getContextFreeVars(context: Context): Set<number> {
+  const s = new Set<number>();
+  for (const t of Object.values(context)) {
+    for (const id of getTypeFreeVars(t)) {
+      s.add(id);
+    }
+  }
+  return s;
+}
+
+export function generalize(t: Type, context: Context = {}): Type {
+  const ctxFreeVars = getContextFreeVars(context);
+  let nextId = 0;
+  const bound = new Map<number, number>();
+
+  function recur(t: Type): Type {
+    if (t.type === "named") {
+      return {
+        type: "named",
+        name: t.name,
+        args: t.args.map(recur),
+      };
+    }
+
+    if (t.type === "fn") {
+      throw new Error("TODO generalize fn");
+    }
+
+    const resolvedT = t.var.resolve();
+    switch (resolvedT.type) {
+      case "quantified":
+        throw new Error("[unreachable] cannot generalize polytype");
+      case "bound":
+        return recur(resolvedT.value);
+      case "unbound": {
+        if (ctxFreeVars.has(resolvedT.id)) {
+          return t;
+        }
+
+        const thisId = bound.get(resolvedT.id) ?? nextId++;
+        bound.set(resolvedT.id, thisId);
+        return TVar.quantified(thisId).asType();
+      }
+    }
+  }
+
+  return recur(t);
+}
+
+export function instantiate(t: Type): Type {
+  const instantiated = new Map<number, TVar>();
+
+  function recur(t: Type): Type {
+    if (t.type === "named") {
+      return {
+        type: "named",
+        name: t.name,
+        args: t.args.map(recur),
+      };
+    }
+
+    if (t.type === "fn") {
+      throw new Error("TODO instantiate fn");
+    }
+
+    const resolvedT = t.var.resolve();
+    switch (resolvedT.type) {
+      case "bound":
+      case "unbound":
+        return t;
+      case "quantified": {
+        const lookup = instantiated.get(resolvedT.id);
+        if (lookup === undefined) {
+          const fresh = TVar.fresh();
+          instantiated.set(resolvedT.id, fresh);
+          return fresh.asType();
+        }
+        return lookup.asType();
+      }
+    }
+  }
+
+  return recur(t);
+}
