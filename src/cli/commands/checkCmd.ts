4171493b24406ce1b978c1a0cbb41c0955fdb94f
Alessandro Scandone <alescandone@gmail.com>
2024-02-05 04:56:45 +0100

    fixed await

--- src/cli/commands/checkCmd.ts
+++ src/cli/commands/checkCmd.ts
@@ -1,7 +1,7 @@
 import { check } from "../common";
 
-export function checkCmd(path: string) {
-  const program = check(path);
+export async function checkCmd(path: string) {
+  const program = await check(path);
   if (program === undefined) {
     return;
   }
