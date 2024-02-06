2acfe4a313280ec6c1c4399c3d187dbade767820
Alessandro Scandone <alescandone@gmail.com>
2024-02-06 02:33:26 +0100

    added colors

--- src/cli/utils/colors.ts
+++ src/cli/utils/colors.ts
@@ -1,3 +1,6 @@
+// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+
+export const FgBlue = "\x1b[34m";
 export const FgRed = "\x1b[31m";
 export const Reset = "\x1b[0m";
 export const FgBlack = "\x1b[30m";
