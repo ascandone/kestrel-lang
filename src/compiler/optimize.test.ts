2f9293d4be806f1094ca105701e6c940798f2896
Alessandro Scandone <alescandone@gmail.com>
2024-02-24 16:53:47 +0100

    Applying compiler optimizations (#8)

---
+++ src/compiler/optimize.test.ts
@@ -0,0 +1,210 @@
+import { describe, expect, test } from "vitest";
+import { format } from "../formatter";
+import { unsafeParse } from "../parser";
+import { optimize } from "./optimize";
+import { typecheck } from "../typecheck";
+
+describe("constant folding", () => {
+  test("leave optimized program as it is", () => {
+    expect("let x = 42").toOptimizeAs("let x = 42");
+  });
+
+  test("fold constants (one step)", () => {
+    expect("let x = 1 + 2").toOptimizeAs("let x = 3");
+    expect("let x = 2 * 3").toOptimizeAs("let x = 6");
+    expect("let x = 1.3 +. 2.4").toOptimizeAs("let x = 3.7");
+    expect(`let x = "a" <> "b"`).toOptimizeAs(`let x = "ab"`);
+    expect("let x = 10 - 1").toOptimizeAs("let x = 9");
+    expect("let x = 10.5 -. 1.2").toOptimizeAs("let x = 9.3");
+  });
+
+  test("fold constants (two steps)", () => {
+    expect("let x = (1 + 1) * 4").toOptimizeAs("let x = 8");
+  });
+});
+
+describe("fold iif", () => {
+  test("with no args", () => {
+    expect(`
+    let a = fn { x * y } ()
+`).toOptimizeAs(`
+let a = x * y
+  `);
+  });
+
+  test("with many args", () => {
+    expect(`
+    let a = fn x, y { x * x * y * y } (f(), g())
+`).toOptimizeAs(`
+let a = {
+  let x = f();
+  let y = g();
+  x * x * y * y
+}
+  `);
+  });
+});
+
+describe("inline let bindings", () => {
+  test("does not apply to recursive functions that are only used once", () => {
+    expect(`
+    let glb = {
+      let rec = fn x { rec(x) };
+      rec(10)
+    }
+    `).toOptimizeAs(`
+let glb = {
+  let rec = fn x {
+    rec(x)
+  };
+  rec(10)
+}
+`);
+  });
+
+  test("remove unused bindings", () => {
+    expect(`
+let glb = {
+  let unused = f();
+  g()
+}
+`).toOptimizeAs(`
+let glb = g()
+`);
+  });
+
+  test("inline let binding when is only used once", () => {
+    expect(`
+let g = {
+  let x = f();
+  g(x)
+}
+    `).toOptimizeAs(`
+let g = g(f())
+`);
+  });
+});
+
+test("apply opt to many statements", () => {
+  expect(`
+    let x = 1 + 2
+    let y = 10 + 20
+  `).toOptimizeAs(`
+let x = 3
+
+let y = 30
+  `);
+});
+
+test("optimizations apply to nodes nested within an application", () => {
+  expect(`
+    let x = f(f(1 + 2))
+  `).toOptimizeAs(`
+    let x = f(f(3))
+  `);
+});
+
+test("optimizations apply to nodes nested in a fn body", () => {
+  expect(`
+    let x = fn { 1 + 2 }
+  `).toOptimizeAs(`
+let x = fn {
+  3
+}
+  `);
+});
+
+test("optimizations apply to nodes nested in an if condition", () => {
+  expect(`
+let x =
+  if 1 + 2 {
+    10 + 20
+  } else {
+    100 + 200
+  }
+  `).toOptimizeAs(`
+let x =
+  if 3 {
+    30
+  } else {
+    300
+  }
+  `);
+});
+
+test("optimizations apply to nodes nested in an let expr", () => {
+  expect(`
+let x = {
+  let a = f(1 + 10);
+  1 + 10 + a
+}
+  `).toOptimizeAs(`
+let x = 11 + f(11)
+`);
+});
+
+test("optimizations apply to nodes nested in match expr", () => {
+  expect(`
+let x = match 1 + 2 {
+  _ => 10 + 20
+}
+  `).toOptimizeAs(`
+let x = match 3 {
+  _ => 30,
+}
+`);
+});
+
+test("function inlining example", () => {
+  expect(`
+  let glb = {
+    let add1 = fn x { x + 1 };
+    add1(100)
+  }
+  `).toOptimizeAs(`let glb = 101`);
+
+  expect(`
+  let glb = {
+    let maybe_map = fn x, f {
+      match x {
+        Nothing => Nothing,
+        Just(v) => f(v),
+      }
+    };
+    maybe_map(Just(42), fn x { x + 1 })
+  }
+  `).toOptimizeAs(`
+let glb = match Just(42) {
+  Nothing => Nothing,
+  Just(v) => v + 1,
+}
+`);
+});
+
+interface CustomMatchers<R = unknown> {
+  toOptimizeAs: (formatted: string) => R;
+}
+
+declare module "vitest" {
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
+
+expect.extend({
+  toOptimizeAs(received, expected) {
+    const parsedSrc = unsafeParse(received);
+    const [typed] = typecheck("Main", parsedSrc, {}, []);
+
+    const optimized = optimize(typed);
+    const optFormatted = format(optimized).trimStart().trimEnd();
+
+    const expectedNormalized = expected.trimStart().trimEnd();
+
+    return {
+      pass: optFormatted === expectedNormalized,
+      expected: expectedNormalized,
+      actual: optFormatted,
+      message: () => `The given program is not optimized as expected`,
+    };
+  },
+});
---
+++ src/compiler/optimize.ts
@@ -0,0 +1,320 @@
+import { Binding, ConstLiteral } from "../parser";
+import { TypeMeta, TypedExpr, TypedModule } from "../typecheck";
+
+type Optimization = (src: TypedExpr) => TypedExpr | undefined;
+
+function foldInfix(
+  src: TypedExpr & { type: "application" },
+  fold: (l: ConstLiteral, r: ConstLiteral) => ConstLiteral,
+): TypedExpr | undefined {
+  const [left, right] = src.args;
+  if (left?.type !== "constant" || right?.type !== "constant") {
+    return undefined;
+  }
+
+  return {
+    type: "constant",
+    value: fold(left.value, right.value),
+    span: src.span,
+    $: src.$,
+  };
+}
+
+const constantFolding: Optimization = (src) => {
+  switch (src.type) {
+    case "application":
+      if (src.caller.type !== "identifier") {
+        return undefined;
+      }
+
+      switch (src.caller.name) {
+        case "+":
+        case "+.":
+        case "<>":
+          return foldInfix(src, (l, r) => ({
+            type: l.type,
+            value:
+              //@ts-ignore
+              l.value + r.value,
+          }));
+
+        case "*":
+          return foldInfix(src, (l, r) => ({
+            type: "int",
+            value:
+              //@ts-ignore
+              l.value * r.value,
+          }));
+
+        case "-":
+          return foldInfix(src, (l, r) => ({
+            type: "int",
+            value:
+              //@ts-ignore
+              l.value - r.value,
+          }));
+
+        case "-.":
+          return foldInfix(src, (l, r) => ({
+            type: "float",
+            value:
+              //@ts-ignore
+              l.value - r.value,
+          }));
+
+        default:
+          return undefined;
+      }
+  }
+};
+
+const iifFolding: Optimization = (src) => {
+  if (src.type !== "application") {
+    return undefined;
+  }
+
+  if (src.caller.type !== "fn") {
+    return undefined;
+  }
+
+  const zipped = src.caller.params.map<[Binding<TypeMeta>, TypedExpr]>(
+    (param, index) => [param, src.args[index]!] as const,
+  );
+
+  return zipped.reduceRight(
+    (acc, [binding, arg]): TypedExpr => ({
+      type: "let",
+      span: src.span,
+      $: src.$,
+      binding,
+      body: acc,
+      value: arg,
+    }),
+    src.caller.body,
+  );
+};
+
+const inlineLetExpr: Optimization = (src) => {
+  if (src.type !== "let") {
+    return undefined;
+  }
+
+  const isRec = countBindingUsages(src.binding, src.value);
+  if (isRec) {
+    return undefined;
+  }
+
+  const count = countBindingUsages(src.binding, src.body);
+  if (count === 0) {
+    return src.body;
+  } else if (count === 1) {
+    return substituteBinding(src.binding, src.value, src.body);
+  }
+
+  return undefined;
+};
+
+const OPTIMIZATIONS: Optimization[] = [
+  constantFolding,
+  iifFolding,
+  inlineLetExpr,
+];
+
+class ChangeTracker {
+  public patchedNode = false;
+
+  runOnce(src: TypedExpr): TypedExpr {
+    for (const opt of OPTIMIZATIONS) {
+      const optimizedNode = opt(src);
+      if (optimizedNode !== undefined) {
+        this.patchedNode = true;
+        return optimizeExpr(optimizedNode);
+      }
+    }
+
+    switch (src.type) {
+      case "constant":
+      case "identifier":
+        return src;
+
+      case "application": {
+        return {
+          ...src,
+          caller: this.runOnce(src.caller),
+          args: src.args.map((arg) => this.runOnce(arg)),
+        };
+      }
+
+      case "fn":
+        return {
+          ...src,
+          body: this.runOnce(src.body),
+        };
+
+      case "if":
+        return {
+          ...src,
+          condition: this.runOnce(src.condition),
+          then: this.runOnce(src.then),
+          else: this.runOnce(src.else),
+        };
+      case "let":
+        return {
+          ...src,
+          value: this.runOnce(src.value),
+          body: this.runOnce(src.body),
+        };
+      case "match":
+        return {
+          ...src,
+          expr: this.runOnce(src.expr),
+          clauses: src.clauses.map(([pat, e]) => [pat, this.runOnce(e)]),
+        };
+    }
+  }
+}
+
+function optimizeExpr(src: TypedExpr): TypedExpr {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const optimizer = new ChangeTracker();
+    src = optimizer.runOnce(src);
+    if (!optimizer.patchedNode) {
+      return src;
+    }
+  }
+}
+
+export function optimize(src: TypedModule): TypedModule {
+  return {
+    ...src,
+    declarations: src.declarations.map((decl) => {
+      if (decl.extern) {
+        return decl;
+      }
+      return {
+        ...decl,
+        value: optimizeExpr(decl.value),
+      };
+    }),
+  };
+}
+
+function countBindingUsages(
+  binding: Binding<TypeMeta>,
+  src: TypedExpr,
+): number {
+  let count = 0;
+
+  function visit(src: TypedExpr) {
+    switch (src.type) {
+      case "identifier":
+        if (src.resolution === undefined) {
+          return;
+        }
+        switch (src.resolution.type) {
+          case "local-variable":
+            if (src.resolution.binding === binding) {
+              count++;
+            }
+            return;
+
+          case "global-variable":
+          case "constructor":
+            return;
+        }
+
+      case "constant":
+        return;
+      case "application":
+        visit(src.caller);
+        for (const arg of src.args) {
+          visit(arg);
+        }
+        return;
+
+      case "let":
+        visit(src.value);
+        visit(src.body);
+        return;
+
+      case "fn":
+        visit(src.body);
+        return;
+
+      case "match":
+        visit(src.expr);
+        for (const [, expr] of src.clauses) {
+          visit(expr);
+        }
+        return;
+      case "if":
+        visit(src.condition);
+        visit(src.then);
+        visit(src.else);
+        return;
+    }
+  }
+
+  visit(src);
+
+  return count;
+}
+
+function substituteBinding(
+  binding: Binding<TypeMeta>,
+  with_: TypedExpr,
+  src: TypedExpr,
+): TypedExpr {
+  switch (src.type) {
+    case "identifier":
+      if (src.resolution === undefined) {
+        return src;
+      } else if (src.resolution.type !== "local-variable") {
+        return src;
+      } else if (src.resolution.binding !== binding) {
+        return src;
+      } else {
+        return with_;
+      }
+    case "constant":
+      return src;
+
+    case "application": {
+      return {
+        ...src,
+        caller: substituteBinding(binding, with_, src.caller),
+        args: src.args.map((arg) => substituteBinding(binding, with_, arg)),
+      };
+    }
+
+    case "fn":
+      return {
+        ...src,
+        body: substituteBinding(binding, with_, src.body),
+      };
+
+    case "if":
+      return {
+        ...src,
+        condition: substituteBinding(binding, with_, src.condition),
+        then: substituteBinding(binding, with_, src.then),
+        else: substituteBinding(binding, with_, src.else),
+      };
+    case "let":
+      return {
+        ...src,
+        value: substituteBinding(binding, with_, src.value),
+        body: substituteBinding(binding, with_, src.body),
+      };
+    case "match":
+      return {
+        ...src,
+        expr: substituteBinding(binding, with_, src.expr),
+        clauses: src.clauses.map(([pat, e]) => [
+          pat,
+          substituteBinding(binding, with_, e),
+        ]),
+      };
+  }
+}
