492e5bd1adf6cb078d894d282bca44fad8c3dd06
Alessandro Scandone <alescandone@gmail.com>
2024-06-18 12:34:26 +0200

    refactor

--- src/typecheck/typedAst/inlayHint.ts
+++ src/typecheck/typedAst/inlayHint.ts
@@ -7,82 +7,88 @@ export type InlayHint = {
   paddingLeft: boolean;
 };
 
+class InlayHintBuf {
+  public inlayHints: InlayHint[] = [];
+  constructor(private readonly scheme: TypeScheme) {}
+
+  inlayHintsOfExpr(ast: TypedExpr): void {
+    switch (ast.type) {
+      case "syntax-err":
+      case "constant":
+      case "identifier":
+        return;
+
+      case "application": {
+        const resolved = ast.caller.$.resolve();
+        if (ast.isPipe) {
+          if (resolved.type !== "bound" || resolved.value.type !== "fn") {
+            // invalid pipe
+            return;
+          }
+
+          const [arg] = ast.args;
+          if (arg === undefined) {
+            return;
+          }
+
+          this.inlayHints.push({
+            label: typeToString(arg.$.asType(), this.scheme),
+            offset: arg.span[1],
+            paddingLeft: true,
+          });
+
+          this.inlayHints.push({
+            label: typeToString(resolved.value.return, this.scheme),
+            offset: ast.span[1],
+            paddingLeft: true,
+          });
+        }
+
+        for (const arg of ast.args) {
+          this.inlayHintsOfExpr(arg);
+        }
+        return;
+      }
+
+      case "fn":
+        this.inlayHintsOfExpr(ast.body);
+        return;
+
+      case "list-literal":
+        for (const value of ast.values) {
+          this.inlayHintsOfExpr(value);
+        }
+        return;
+
+      case "let":
+        this.inlayHintsOfExpr(ast.body);
+        this.inlayHintsOfExpr(ast.value);
+        return;
+
+      case "if":
+        this.inlayHintsOfExpr(ast.then);
+        this.inlayHintsOfExpr(ast.condition);
+        this.inlayHintsOfExpr(ast.else);
+        return;
+
+      case "match":
+        this.inlayHintsOfExpr(ast.expr);
+        for (const [_p, expr] of ast.clauses) {
+          this.inlayHintsOfExpr(expr);
+        }
+        return;
+    }
+  }
+}
+
 export function getInlayHints(module: TypedModule): InlayHint[] {
   return module.declarations.flatMap<InlayHint>((d) => {
     if (d.extern) {
       return [];
     }
-    return [...inlayHintsOfExpr(d.value, d.scheme)];
+    const buf = new InlayHintBuf(d.scheme);
+
+    buf.inlayHintsOfExpr(d.value);
+    return buf.inlayHints;
   });
-}
-
-function* inlayHintsOfExpr(
-  ast: TypedExpr,
-  scheme: TypeScheme,
-): Generator<InlayHint> {
-  switch (ast.type) {
-    case "syntax-err":
-    case "constant":
-    case "identifier":
-      return;
-
-    case "application": {
-      const resolved = ast.caller.$.resolve();
-      if (ast.isPipe) {
-        if (resolved.type !== "bound" || resolved.value.type !== "fn") {
-          // invalid pipe
-          return;
-        }
-
-        const [arg] = ast.args;
-        if (arg === undefined) {
-          return;
-        }
-
-        yield {
-          label: typeToString(arg.$.asType(), scheme),
-          offset: arg.span[1],
-          paddingLeft: true,
-        };
-
-        yield {
-          label: typeToString(resolved.value.return, scheme),
-          offset: ast.span[1],
-          paddingLeft: true,
-        };
-      }
-
-      for (const arg of ast.args) {
-        yield* inlayHintsOfExpr(arg, scheme);
-      }
-      return;
-    }
-
-    case "fn":
-      return yield* inlayHintsOfExpr(ast.body, scheme);
-
-    case "list-literal":
-      for (const value of ast.values) {
-        yield* inlayHintsOfExpr(value, scheme);
-      }
-      return;
-
-    case "let":
-      yield* inlayHintsOfExpr(ast.body, scheme);
-      yield* inlayHintsOfExpr(ast.value, scheme);
-      return;
-
-    case "if":
-      yield* inlayHintsOfExpr(ast.then, scheme);
-      yield* inlayHintsOfExpr(ast.condition, scheme);
-      yield* inlayHintsOfExpr(ast.else, scheme);
-      return;
-
-    case "match":
-      yield* inlayHintsOfExpr(ast.expr, scheme);
-      for (const [_p, expr] of ast.clauses) {
-        yield* inlayHintsOfExpr(expr, scheme);
-      }
-      return;
-  }
 }
