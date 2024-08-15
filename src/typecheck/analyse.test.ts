b04d68f5b83f6f5a23bb0b1cee3f8f985e81f98b
ascandone <alescandone@gmail.com>
2024-08-15 12:50:26 +0200

    more infer literals tests

--- src/typecheck/analyse.test.ts
+++ src/typecheck/analyse.test.ts
@@ -11,6 +11,48 @@ test("infer int", () => {
   });
 });
 
+test("infer float", () => {
+  const a = new Analysis(
+    "Main",
+    `
+      pub let x = 42.2
+    `,
+  );
+
+  expect(a.errors).toEqual([]);
+  expect(getTypes(a)).toEqual({
+    x: "Float",
+  });
+});
+
+test("infer string", () => {
+  const a = new Analysis(
+    "Main",
+    `
+        pub let x = "abc"
+      `,
+  );
+
+  expect(a.errors).toEqual([]);
+  expect(getTypes(a)).toEqual({
+    x: "String",
+  });
+});
+
+test("infer char", () => {
+  const a = new Analysis(
+    "Main",
+    `
+          pub let x = 'a'
+        `,
+  );
+
+  expect(a.errors).toEqual([]);
+  expect(getTypes(a)).toEqual({
+    x: "Char",
+  });
+});
+
 function getTypes(a: Analysis): Record<string, string> {
   const kvs = [...a.getPublicDeclarations()].map((decl) => {
     return [decl.binding.name, typeToString(a.getType(decl.binding))];
