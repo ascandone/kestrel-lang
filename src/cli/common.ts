c8d02e3e4744875277e6378d2730c512cefb2821
ascandone <alescandone@gmail.com>
2024-08-19 23:58:23 +0200

    switched compiler

--- src/cli/common.ts
+++ src/cli/common.ts
@@ -7,7 +7,7 @@ import {
   UntypedProject,
 } from "../typecheck";
 import { exit } from "node:process";
-import { compileProject, defaultEntryPoint } from "../compiler";
+import { compileProject, defaultEntryPoint } from "../compiler/compiler-babel";
 import { col } from "../utils/colors";
 import { Config, readConfig } from "./config";
 import { join } from "node:path";
