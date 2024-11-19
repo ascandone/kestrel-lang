b54c3b77ad0bd00fc4fdb32a71b1225625ea6388
ascandone <alescandone@gmail.com>
2024-11-19 16:09:46 +0100

    added new Type implementation

---
+++ src/__test__/commonTypes.ts
@@ -0,0 +1,39 @@
+import { Type } from "../type/type";
+
+export const string: Type = { tag: "Named", name: "String", args: [] };
+export const num: Type = { tag: "Named", name: "Num", args: [] };
+export const bool: Type = { tag: "Named", name: "Bool", args: [] };
+export const list = (x: Type): Type => ({
+  tag: "Named",
+  name: "List",
+  args: [x],
+});
+export const tuple = (...ts: Type[]): Type => ({
+  tag: "Named",
+  name: `Tuple${ts.length}`,
+  args: ts,
+});
+export const unit: Type = {
+  tag: "Named",
+  name: `Unit`,
+  args: [],
+};
+export const maybe = (...ts: Type[]): Type => ({
+  tag: "Named",
+  name: "Maybe",
+  args: ts,
+});
+
+export const fn = (args: Type[], return_: Type): Type => ({
+  tag: "Fn",
+  args,
+  return: return_,
+});
+
+export const $ = (id: number): Type => ({ tag: "Var", id });
+
+export const _0 = $(0),
+  _1 = $(1),
+  _2 = $(2),
+  _3 = $(3),
+  _4 = $(4);
