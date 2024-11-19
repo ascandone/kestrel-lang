4f8d84a86bbc1e7222f3e49e9aa8cddb3171933b
ascandone <alescandone@gmail.com>
2024-11-19 15:09:14 +0100

    allow cyclic deps

--- src/data/defaultMap.ts
+++ src/data/defaultMap.ts
@@ -1,14 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function defaultRecordGet<K extends keyof any, T>(
-  r: Record<K, T>,
+export function defaultRecordGet<K extends keyof any, V>(
+  r: Record<K, NonNullable<V>>,
   k: K,
-  makeDefault: () => T,
-): T {
+  makeDefault: () => NonNullable<V>,
+): V {
   const lookup = r[k];
   if (lookup !== undefined) {
     return lookup;
   }
   const defaultValue = makeDefault();
   r[k] = defaultValue;
+  return defaultValue;
+}
+
+export function defaultWeakmapGet<K extends object, V>(
+  wm: WeakMap<K, NonNullable<V>>,
+  k: K,
+  makeDefault: () => NonNullable<V>,
+) {
+  const lookup = wm.get(k);
+  if (lookup !== undefined) {
+    return lookup;
+  }
+
+  const defaultValue = makeDefault();
+  wm.set(k, defaultValue);
   return defaultValue;
 }
