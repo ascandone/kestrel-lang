c73809c0433404a30f4916212ea009a6c48b38c3
Alessandro Scandone <alescandone@gmail.com>
2025-08-08 20:39:23 +0200

    added tests

--- src/typecheck/typecheck.test.ts
+++ src/typecheck/typecheck.test.ts
@@ -2715,7 +2715,7 @@ describe.todo("modules", () => {
   });
 });
 
-describe.todo("typecheck project", () => {
+describe("typecheck project", () => {
   test("single import", () => {
     const project = typecheckProject(
       {
