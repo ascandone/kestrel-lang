0f437672aa9a2690889693bfb5fd2fa175b5f462
ascandone <alescandone@gmail.com>
2025-09-14 18:25:30 +0200

    added dbg utilities

--- src/typecheck/__test__/dbtDecisionTree.ts
+++ src/typecheck/__test__/dbtDecisionTree.ts
@@ -1,4 +1,36 @@
 import { DecisionTree, DecisionTreeBinding } from "../exhaustiveness";
+import { TypedMatchPattern } from "../typedAst";
+
+function patToString(pat: TypedMatchPattern): string {
+  switch (pat.type) {
+    case "identifier":
+      return pat.name;
+
+    case "constant":
+      return pat.value.value.toString();
+
+    case "constructor":
+      if (pat.args.length === 0) {
+        return pat.name;
+      }
+      return `${pat.name}(${pat.args.map(patToString).join(", ")})`;
+  }
+}
+
+// we duplicate this code so that we don't have to export the private type
+type PatternMatrix = {
+  patterns: TypedMatchPattern[];
+  action: number;
+}[];
+
+export function dbgMatrix(matrix: PatternMatrix) {
+  console.log("<matrix>");
+  for (const clause of matrix) {
+    const str = clause.patterns.map(patToString).join(", ");
+    console.log(`[${str}] -> ${clause.action}`);
+  }
+  console.log("</matrix>\n");
+}
 
 export function dbgTree(tree: DecisionTree, level: number = 0) {
   const IDENT_SYM = "  ";
@@ -16,11 +48,11 @@ export function dbgTree(tree: DecisionTree, level: number = 0) {
 
   switch (tree.type) {
     case "leaf":
-      console.info(ident, `<leaf>`, tree.action, "</leaf>");
+      console.info(`${ident}<leaf>`, tree.action, "</leaf>");
       break;
 
     case "switch":
-      console.info(ident, `<match subject=${argPatToString(tree.subject)}>`);
+      console.info(`${ident}<match subject=${argPatToString(tree.subject)}>`);
 
       for (const [pat, sub] of tree.clauses) {
         switch (pat.type) {
@@ -31,20 +63,18 @@ export function dbgTree(tree: DecisionTree, level: number = 0) {
                 : "(" + pat.args.map(argPatToString).join(", ") + ")";
 
             console.info(
-              ident + IDENT_SYM,
-              `<ctor pattern=${pat.resolution.declaration.name}::${pat.resolution.variant.name}${ctorArgs}>`,
+              `${ident}${IDENT_SYM}<ctor pattern=${pat.resolution.declaration.name}::${pat.resolution.variant.name}${ctorArgs}>`,
             );
             dbgTree(sub, level + 2);
-            console.info(ident + IDENT_SYM, "</ctor>");
+            console.info(`${ident}${IDENT_SYM}</ctor>`);
             break;
           }
           case "constant":
             console.info(
-              ident + IDENT_SYM,
-              `<const value=${pat.value.value.toString()}>`,
+              `${ident}${IDENT_SYM}<const value=${pat.value.value.toString()}>`,
             );
             dbgTree(sub, level + 2);
-            console.info(ident + IDENT_SYM, "</const>");
+            console.info(`${ident}${IDENT_SYM}</const>`);
             break;
         }
       }
@@ -52,15 +82,14 @@ export function dbgTree(tree: DecisionTree, level: number = 0) {
       if (tree.default !== undefined) {
         const [pat, subTree] = tree.default;
         console.info(
-          ident + IDENT_SYM,
-          `<default binding=${argPatToString(pat)}>`,
+          `${ident}${IDENT_SYM}<default binding=${argPatToString(pat)}>`,
         );
 
         dbgTree(subTree, level + 2);
-        console.info(ident + IDENT_SYM, "</default>");
+        console.info(`${ident}${IDENT_SYM}</default>`);
       }
 
-      console.info(ident, "</match>");
+      console.info(ident + "</match>");
       break;
   }
 }
